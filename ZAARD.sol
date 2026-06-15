2d6bf9f0ae6ba53dd1b23a396090368665a537e3
ZAARD <143908070+figueredo56@users.noreply.github.com>
2026-06-15 19:58:40 -0400

    Refactor ZARDToken contract implementation

--- ZAARD.sol
+++ ZAARD.sol
@@ -1,10 +1,52 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract ZARDToken {
+    string public constant name = "ZARD";
+    string public constant symbol = "ZARD";
+    uint8 public constant decimals = 18;
+    uint256 public constant totalSupply = 200 * 10**18;
 
-contract ZaardToken is ERC20 {
-    constructor() ERC20("Zaard Token", "ZAARD") {
-            _mint(msg.sender, 1000000 * 10 ** decimals());
-                }
-                }
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function transfer(address to, uint256 value) public returns (bool success) {
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf[msg.sender] >= value, "ERC20: transfer amount exceeds balance");
+        
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool success) {
+        require(spender != address(0), "ERC20: approve to the zero address");
+        
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool success) {
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(value <= balanceOf[from], "ERC20: transfer amount exceeds balance");
+        require(value <= allowance[from][msg.sender], "ERC20: transfer amount exceeds allowance");
+        
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+        
+        emit Transfer(from, to, value);
+        return true;
+    }
+}
