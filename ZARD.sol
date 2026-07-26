170b2432ef7e4235f65cea1934210674ef688cf2
ZAARD <143908070+figueredo56@users.noreply.github.com>
2026-07-26 18:01:57 -0400

    Update and rename ZAARD.sol to ZARD.sol

---
+++ ZARD.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ZARD is ERC20 {
+    constructor() ERC20("ZARD", "ZARD") {
+            uint256 totalSupplyTokens = 220 * (10 ** decimals());
+                    _mint(msg.sender, totalSupplyTokens);
+                        }
+                        }
