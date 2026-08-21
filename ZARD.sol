ec47f5c509137f5e8f97ffa9fdb3ec3c66e78121
ZAARD <143908070+figueredo56@users.noreply.github.com>
2026-08-21 14:59:21 -0500

    Refactor ZARDToken contract to ZARD with improvements

--- ZARD.sol
+++ ZARD.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
-contract ZARDToken {
-    string public constant name = "ZARD";
-        string public constant symbol = "ZARD";
-            uint8 public constant decimals = 18;
-                uint256 public constant totalSupply = 200 * 10**18;
+contract ZARD {
+    string public name = "ZARD";
+        string public symbol = "ZARD";
+            uint8 public decimals = 18;
+                uint256 public totalSupply;
 
                     mapping(address => uint256) public balanceOf;
                         mapping(address => mapping(address => uint256)) public allowance;
@@ -14,39 +14,34 @@ contract ZARDToken {
                                 event Approval(address indexed owner, address indexed spender, uint256 value);
 
                                     constructor() {
-                                            balanceOf[msg.sender] = totalSupply;
-                                                    emit Transfer(address(0), msg.sender, totalSupply);
-                                                        }
+                                            totalSupply = 200 * 10**decimals;
+                                                    balanceOf[msg.sender] = totalSupply;
+                                                            emit Transfer(address(0), msg.sender, totalSupply);
+                                                                }
 
-                                                            function transfer(address to, uint256 value) public returns (bool success) {
-                                                                    require(to != address(0), "ERC20: transfer to the zero address");
-                                                                            require(balanceOf[msg.sender] >= value, "ERC20: transfer amount exceeds balance");
-                                                                                    
-                                                                                            balanceOf[msg.sender] -= value;
-                                                                                                    balanceOf[to] += value;
-                                                                                                            
-                                                                                                                    emit Transfer(msg.sender, to, value);
-                                                                                                                            return true;
-                                                                                                                                }
+                                                                    function transfer(address to, uint256 value) public returns (bool) {
+                                                                            require(balanceOf[msg.sender] >= value, "Insufficient balance");
+                                                                                    balanceOf[msg.sender] -= value;
+                                                                                            balanceOf[to] += value;
+                                                                                                    emit Transfer(msg.sender, to, value);
+                                                                                                            return true;
+                                                                                                                }
 
-                                                                                                                                    function approve(address spender, uint256 value) public returns (bool success) {
-                                                                                                                                            require(spender != address(0), "ERC20: approve to the zero address");
-                                                                                                                                                    
-                                                                                                                                                            allowance[msg.sender][spender] = value;
-                                                                                                                                                                    emit Approval(msg.sender, spender, value);
-                                                                                                                                                                            return true;
-                                                                                                                                                                                }
+                                                                                                                    function approve(address spender, uint256 value) public returns (bool) {
+                                                                                                                            allowance[msg.sender][spender] = value;
+                                                                                                                                    emit Approval(msg.sender, spender, value);
+                                                                                                                                            return true;
+                                                                                                                                                }
 
-                                                                                                                                                                                    function transferFrom(address from, address to, uint256 value) public returns (bool success) {
-                                                                                                                                                                                            require(to != address(0), "ERC20: transfer to the zero address");
-                                                                                                                                                                                                    require(value <= balanceOf[from], "ERC20: transfer amount exceeds balance");
-                                                                                                                                                                                                            require(value <= allowance[from][msg.sender], "ERC20: transfer amount exceeds allowance");
-                                                                                                                                                                                                                    
-                                                                                                                                                                                                                            balanceOf[from] -= value;
-                                                                                                                                                                                                                                    balanceOf[to] += value;
-                                                                                                                                                                                                                                            allowance[from][msg.sender] -= value;
-                                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                            emit Transfer(from, to, value);
-                                                                                                                                                                                                                                                                    return true;
-                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                        }
+                                                                                                                                                    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+                                                                                                                                                            require(balanceOf[from] >= value, "Insufficient balance");
+                                                                                                                                                                    require(allowance[from][msg.sender] >= value, "Allowance exceeded");
+                                                                                                                                                                            
+                                                                                                                                                                                    allowance[from][msg.sender] -= value;
+                                                                                                                                                                                            balanceOf[from] -= value;
+                                                                                                                                                                                                    balanceOf[to] += value;
+                                                                                                                                                                                                            
+                                                                                                                                                                                                                    emit Transfer(from, to, value);
+                                                                                                                                                                                                                            return true;
+                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                }
